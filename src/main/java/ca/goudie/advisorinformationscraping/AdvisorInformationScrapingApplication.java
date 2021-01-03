44fa3afca56338d0e1b1707ccb2715bff2e2f434
James Gabriel Goudie <james.g.goudie@gmail.com>
2021-01-03 16:14:26 -0500

    Initial commit with starter spring project

---
+++ src/main/java/ca/goudie/advisorinformationscraping/AdvisorInformationScrapingApplication.java
@@ -0,0 +1,13 @@
+package ca.goudie.advisorinformationscraping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvisorInformationScrapingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvisorInformationScrapingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/goudie/advisorinformationscraping/AdvisorInformationScrapingApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.goudie.advisorinformationscraping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvisorInformationScrapingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
