cbf26450f6185422297c1227982f0f7ee0a33293
James Gabriel Goudie <james.g.goudie@gmail.com>
2021-01-14 00:56:01 -0500

    Removed unused imports

--- src/test/java/ca/goudie/advisorinformationscraping/services/scrapers/GenericScraperAbacoAmTests.java
+++ src/test/java/ca/goudie/advisorinformationscraping/services/scrapers/GenericScraperAbacoAmTests.java
@@ -14,7 +14,6 @@ import java.util.HashSet;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 
