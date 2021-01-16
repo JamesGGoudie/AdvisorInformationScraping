e308f3e0b2b6ba3e0c5c6475fe243e39e1d8b514
James Gabriel Goudie <james.g.goudie@gmail.com>
2021-01-16 17:43:38 -0500

    Turned the confidence scores into a hashmap to have a score for each item found

--- src/main/java/ca/goudie/advisorinformationscraping/models/common/Employee.java
+++ src/main/java/ca/goudie/advisorinformationscraping/models/common/Employee.java
@@ -3,7 +3,9 @@ package ca.goudie.advisorinformationscraping.models.common;
 import lombok.Data;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 
 /**
  * A collection of data about an employee from a website.
@@ -14,11 +16,11 @@ public class Employee {
 	private String name;
 	private String title;
 	private final Collection<String> phones = new HashSet<>();
-	private Float phoneScore;
+	private final Map<String, Float> phoneScores = new HashMap<>();
 	private final Collection<String> emails = new HashSet<>();
-	private Float emailScore;
+	private final Map<String, Float> emailScores = new HashMap<>();
 	private final Collection<String> addresses = new HashSet<>();
-	private Float addressScore;
+	private final Map<String, Float> addressScores = new HashMap<>();
 	private String source;
 
 }
