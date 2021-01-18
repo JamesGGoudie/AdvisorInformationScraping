097158ecf36bd83fd7b40d03a929b165aa1ec3eb
James Gabriel Goudie <james.g.goudie@gmail.com>
2021-01-18 00:54:22 -0500

    Added a unit test for the Prosser Knowles scrape. Also simplified the Abaco scrape test

--- src/test/java/ca/goudie/advisorinformationscraping/services/scrapers/GenericScraperAbacoTests.java
+++ src/test/java/ca/goudie/advisorinformationscraping/services/scrapers/GenericScraperAbacoTests.java
@@ -2,7 +2,6 @@ package ca.goudie.advisorinformationscraping.services.scrapers;
 
 import ca.goudie.advisorinformationscraping.exceptions.ScrapeException;
 import ca.goudie.advisorinformationscraping.models.common.Firm;
-import ca.goudie.advisorinformationscraping.models.common.Employee;
 import ca.goudie.advisorinformationscraping.services.scrapers.generic.GenericScraper;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
@@ -10,11 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Collection;
-import java.util.HashSet;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 
@@ -29,40 +24,25 @@ public class GenericScraperAbacoTests {
 	private WebDriver chromeWebDriver;
 
 	@Test
-	public void shouldScrapeAbacoAm() throws ScrapeException {
-		final String abacoUrl = "http://www.abacoam.com/";
+	public void shouldScrapeAbaco() throws ScrapeException {
+		final String landingPageUrl = "http://www.abacoam.com/";
 		final String countryCode = "GB";
 
 		WebDriver mockDriver = spy(this.chromeWebDriver);
 
-		final Firm f = this.genericScraper.scrapeWebsite(mockDriver,
-				abacoUrl,
+		final Firm aFirm = this.genericScraper.scrapeWebsite(mockDriver,
+				landingPageUrl,
 				countryCode);
 
-		verify(mockDriver).get(abacoUrl);
+		verify(mockDriver).get(landingPageUrl);
 
-		assertEquals("www.abacoam.com", f.getFirmUrl());
-		assertEquals("www.abacoam.com", f.getSource());
+		final Firm eFirm = new Firm();
+		eFirm.setFirmUrl("www.abacoam.com");
+		eFirm.setSource("www.abacoam.com");
+		eFirm.getPhones().add("+44 20 3031 9184");
+		eFirm.getEmails().add("Abaco.AssetManagement@abacoam.com");
 
-		final Collection<String> ePhones = new HashSet<>();
-		ePhones.add("+44 20 3031 9184");
-
-		assertEquals(ePhones, f.getPhones());
-
-		final Collection<String> eEmails = new HashSet<>();
-		eEmails.add("Abaco.AssetManagement@abacoam.com");
-
-		assertEquals(eEmails, f.getEmails());
-
-		final Collection<String> aAddresses = f.getAddresses();
-
-		assertNotNull(aAddresses);
-		assertEquals(0, aAddresses.size());
-
-		final Collection<Employee> aEmployees = f.getEmployees();
-
-		assertNotNull(aEmployees);
-		assertEquals(0, aEmployees.size());
+		assertEquals(eFirm, aFirm);
 	}
 
 }
---
+++ src/test/java/ca/goudie/advisorinformationscraping/services/scrapers/GenericScraperProsserKnowlesTests.java
@@ -0,0 +1,257 @@
+package ca.goudie.advisorinformationscraping.services.scrapers;
+
+import ca.goudie.advisorinformationscraping.exceptions.ScrapeException;
+import ca.goudie.advisorinformationscraping.models.common.Employee;
+import ca.goudie.advisorinformationscraping.models.common.Firm;
+import ca.goudie.advisorinformationscraping.services.scrapers.generic.GenericScraper;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest
+public class GenericScraperProsserKnowlesTests {
+
+	@Autowired
+	private GenericScraper genericScraper;
+
+	@Autowired
+	@Qualifier("chromeWebDriver")
+	private WebDriver chromeWebDriver;
+
+	@Test
+	public void shouldScrapeProsserKnowles() throws ScrapeException {
+		final String landingPageUrl = "https://www.prosserknowles.co.uk/";
+		final String countryCode = "GB";
+
+		WebDriver mockDriver = spy(this.chromeWebDriver);
+
+		final Firm aFirm = this.genericScraper.scrapeWebsite(mockDriver,
+				landingPageUrl,
+				countryCode);
+
+		verify(mockDriver).get(landingPageUrl);
+		verify(mockDriver).get("https://www.prosserknowles.co.uk/about-us/our-team/");
+		verify(mockDriver).get("https://www.prosserknowles.co.uk/about-us/our-team/directors/nick-aston/");
+		verify(mockDriver).get("https://www.prosserknowles.co.uk/about-us/our-team/directors/nick-broughton/");
+		verify(mockDriver).get("https://www.prosserknowles.co.uk/about-us/our-team/consultants/dale-gough/");
+		verify(mockDriver).get("https://www.prosserknowles.co.uk/about-us/our-team/consultants/leighton-parkes/");
+		verify(mockDriver).get("https://www.prosserknowles.co.uk/about-us/our-team/directors/andrew-prosser/");
+		verify(mockDriver).get("https://www.prosserknowles.co.uk/about-us/our-team/consultants/philip-batson/");
+		verify(mockDriver).get("https://www.prosserknowles.co.uk/about-us/our-team/consultants/jonathan-bissett/");
+		verify(mockDriver).get("https://www.prosserknowles.co.uk/about-us/our-team/consultants/vanessa-coates/");
+		verify(mockDriver).get("https://www.prosserknowles.co.uk/about-us/our-team/consultants/aaron-dakin/");
+		verify(mockDriver).get("https://www.prosserknowles.co.uk/about-us/our-team/consultants/laura-evans/");
+		verify(mockDriver).get("https://www.prosserknowles.co.uk/about-us/our-team/client-services-team/jack-la-fave/");
+		verify(mockDriver).get("https://www.prosserknowles.co.uk/about-us/our-team/consultants/anna-lawrence/");
+		verify(mockDriver).get("https://www.prosserknowles.co.uk/about-us/our-team/consultants/goss-lumsden/");
+		verify(mockDriver).get("https://www.prosserknowles.co.uk/about-us/our-team/consultants/daniel-morris/");
+		verify(mockDriver).get("https://www.prosserknowles.co.uk/about-us/our-team/consultants/callum-pye/");
+		verify(mockDriver).get("https://www.prosserknowles.co.uk/about-us/our-team/consultants/rachel-robb/");
+
+		final Firm eFirm = new Firm();
+
+		eFirm.setFirmUrl("www.prosserknowles.co.uk");
+		eFirm.setSource("www.prosserknowles.co.uk");
+		eFirm.getPhones().add("01562 829 222");
+		eFirm.getEmails().add("enquiries@prosserknowles.co.uk");
+
+		Employee eEmployee;
+
+		eEmployee = new Employee();
+
+		eEmployee.setName("Nick Aston");
+		eEmployee.setTitle("Managing Director & Chartered Financial Planner");
+		eEmployee.getPhones().put("07494 986 720", 1.0f);
+		eEmployee.getPhones().put("01452 260 840", 0.25f);
+		eEmployee.getEmails().put("nick@prosserknowles.co.uk", 1.0f);
+
+		eEmployee.setSource("www.prosserknowles.co.uk/about-us/our-team/directors/nick-aston");
+
+		eFirm.getEmployees().add(eEmployee);
+
+		eEmployee = new Employee();
+
+		eEmployee.setName("Nick Broughton");
+		eEmployee.setTitle("Director, Financial Planning & Trust Specialist");
+		eEmployee.getPhones().put("07494 986 717", 1.0f);
+		eEmployee.getPhones().put("01905 619 100", 0.2f);
+		eEmployee.getEmails().put("nickbroughton@prosserknowles.co.uk", 1.0f);
+
+		eEmployee.setSource("www.prosserknowles.co.uk/about-us/our-team/directors/nick-broughton");
+
+		eFirm.getEmployees().add(eEmployee);
+
+		eEmployee = new Employee();
+
+		eEmployee.setName("Dale Gough");
+		eEmployee.setTitle("Director & Financial Planning Consultant");
+		eEmployee.getPhones().put("01905 619 100", 0.2f);
+		eEmployee.getEmails().put("dale@prosserknowles.co.uk", 1.0f);
+
+		eEmployee.setSource("www.prosserknowles.co.uk/about-us/our-team/consultants/dale-gough");
+
+		eFirm.getEmployees().add(eEmployee);
+
+		eEmployee = new Employee();
+
+		eEmployee.setName("Leighton Parkes");
+		eEmployee.setTitle("Director & Financial Planning Consultant");
+		eEmployee.getPhones().put("01562 829 222", 0.071428575f);
+		eEmployee.getEmails().put("leighton@prosserknowles.co.uk", 1.0f);
+
+		eEmployee.setSource("www.prosserknowles.co.uk/about-us/our-team/consultants/leighton-parkes");
+
+		eFirm.getEmployees().add(eEmployee);
+
+		eEmployee = new Employee();
+
+		eEmployee.setName("Andrew Prosser");
+		eEmployee.setTitle("Chairman & Financial Planning Consultant");
+		eEmployee.getPhones().put("01562 829 222", 0.071428575f);
+		eEmployee.getEmails().put("andrew@prosserknowles.co.uk", 1.0f);
+
+		eEmployee.setSource("www.prosserknowles.co.uk/about-us/our-team/directors/andrew-prosser");
+
+		eFirm.getEmployees().add(eEmployee);
+
+		eEmployee = new Employee();
+
+		eEmployee.setName("Philip Batson");
+		eEmployee.setTitle("Financial Planning Consultant");
+		eEmployee.getPhones().put("07943 918 730", 1.0f);
+		eEmployee.getPhones().put("01562 829 222", 0.071428575f);
+		eEmployee.getEmails().put("philip@prosserknowles.co.uk", 1.0f);
+
+		eEmployee.setSource("www.prosserknowles.co.uk/about-us/our-team/consultants/philip-batson");
+
+		eFirm.getEmployees().add(eEmployee);
+
+		eEmployee = new Employee();
+
+		eEmployee.setName("Jonathan Bissett");
+		eEmployee.setTitle("Financial Planning Consultant");
+		eEmployee.getPhones().put("07494 986 713", 1.0f);
+		eEmployee.getPhones().put("01452 260 840", 0.25f);
+		eEmployee.getEmails().put("jonathan@prosserknowles.co.uk", 1.0f);
+
+		eEmployee.setSource("www.prosserknowles.co.uk/about-us/our-team/consultants/jonathan-bissett");
+
+		eFirm.getEmployees().add(eEmployee);
+
+		eEmployee = new Employee();
+
+		eEmployee.setName("Vanessa Coates");
+		eEmployee.setTitle("Financial Planning Consultant");
+		eEmployee.getPhones().put("07943 922 989", 1.0f);
+		eEmployee.getPhones().put("01452 260 840", 0.25f);
+		eEmployee.getEmails().put("vanessa@prosserknowles.co.uk", 1.0f);
+
+		eEmployee.setSource("www.prosserknowles.co.uk/about-us/our-team/consultants/vanessa-coates");
+
+		eFirm.getEmployees().add(eEmployee);
+
+		eEmployee = new Employee();
+
+		eEmployee.setName("Aaron Dakin");
+		eEmployee.setTitle("Financial Planning Consultant");
+		eEmployee.getPhones().put("07932 837 627", 1.0f);
+		eEmployee.getPhones().put("01905 619 100", 0.2f);
+		eEmployee.getEmails().put("aaron@prosserknowles.co.uk", 1.0f);
+
+		eEmployee.setSource("www.prosserknowles.co.uk/about-us/our-team/consultants/aaron-dakin");
+
+		eFirm.getEmployees().add(eEmployee);
+
+		eEmployee = new Employee();
+
+		eEmployee.setName("Laura Evans");
+		eEmployee.setTitle("Financial Planning Consultant");
+		eEmployee.getPhones().put("07943 916 516", 1.0f);
+		eEmployee.getPhones().put("01562 829 222", 0.071428575f);
+		eEmployee.getEmails().put("laura@prosserknowles.co.uk", 1.0f);
+
+		eEmployee.setSource("www.prosserknowles.co.uk/about-us/our-team/consultants/laura-evans");
+
+		eFirm.getEmployees().add(eEmployee);
+
+		eEmployee = new Employee();
+
+		eEmployee.setName("Jack La Fave");
+		eEmployee.setTitle("Financial Planning Consultant");
+		eEmployee.getPhones().put("01562 829 222", 0.071428575f);
+		eEmployee.getEmails().put("jack@prosserknowles.co.uk", 1.0f);
+
+		eEmployee.setSource("www.prosserknowles.co.uk/about-us/our-team/client-services-team/jack-la-fave");
+
+		eFirm.getEmployees().add(eEmployee);
+
+		eEmployee = new Employee();
+
+		eEmployee.setName("Anna Lawrence");
+		eEmployee.setTitle("Chartered Financial Planner");
+		eEmployee.getPhones().put("07943 923 508", 1.0f);
+		eEmployee.getPhones().put("01452 260 840", 0.25f);
+		eEmployee.getEmails().put("anna@prosserknowles.co.uk", 1.0f);
+
+		eEmployee.setSource("www.prosserknowles.co.uk/about-us/our-team/consultants/anna-lawrence");
+
+		eFirm.getEmployees().add(eEmployee);
+
+		eEmployee = new Employee();
+
+		eEmployee.setName("Goss Lumsden");
+		eEmployee.setTitle("Financial Planning Consultant");
+		eEmployee.getPhones().put("07494 986 714", 1.0f);
+		eEmployee.getPhones().put("01562 829 222", 0.071428575f);
+		eEmployee.getEmails().put("goss@prosserknowles.co.uk", 1.0f);
+
+		eEmployee.setSource("www.prosserknowles.co.uk/about-us/our-team/consultants/goss-lumsden");
+
+		eFirm.getEmployees().add(eEmployee);
+
+		eEmployee = new Employee();
+
+		eEmployee.setName("Daniel Morris");
+		eEmployee.setTitle("Financial Planning Consultant");
+		eEmployee.getPhones().put("07494 986 730", 1.0f);
+		eEmployee.getPhones().put("01562 829 222", 0.071428575f);
+		eEmployee.getEmails().put("daniel@prosserknowles.co.uk", 1.0f);
+
+		eEmployee.setSource("www.prosserknowles.co.uk/about-us/our-team/consultants/daniel-morris");
+
+		eFirm.getEmployees().add(eEmployee);
+
+		eEmployee = new Employee();
+
+		eEmployee.setName("Callum Pye");
+		eEmployee.setTitle("Financial Planning Consultant");
+		eEmployee.getPhones().put("07494 986 711", 1.0f);
+		eEmployee.getPhones().put("01905 619 100", 0.2f);
+		eEmployee.getEmails().put("callum@prosserknowles.co.uk", 1.0f);
+
+		eEmployee.setSource("www.prosserknowles.co.uk/about-us/our-team/consultants/callum-pye");
+
+		eFirm.getEmployees().add(eEmployee);
+
+		eEmployee = new Employee();
+
+		eEmployee.setName("Rachel Robb");
+		eEmployee.setTitle("Financial Planning Consultant");
+		eEmployee.getPhones().put("07494 986 716", 1.0f);
+		eEmployee.getPhones().put("01905 619 100", 0.2f);
+		eEmployee.getEmails().put("rachel@prosserknowles.co.uk", 1.0f);
+
+		eEmployee.setSource("www.prosserknowles.co.uk/about-us/our-team/consultants/rachel-robb");
+
+		eFirm.getEmployees().add(eEmployee);
+
+		assertEquals(eFirm, aFirm);
+	}
+
+}
