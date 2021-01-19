715d55c299e3475471a11e8ec02be344c1da468e
James Gabriel Goudie <james.g.goudie@gmail.com>
2021-01-19 17:36:41 -0500

    Moved run-controller logic into a dedicated service. Removed scraper facade

---
+++ src/main/java/ca/goudie/advisorinformationscraping/services/RunService.java
@@ -0,0 +1,91 @@
+package ca.goudie.advisorinformationscraping.services;
+
+import ca.goudie.advisorinformationscraping.dto.Firm;
+import ca.goudie.advisorinformationscraping.dto.ScrapeResult;
+import ca.goudie.advisorinformationscraping.exceptions.ScrapeException;
+import ca.goudie.advisorinformationscraping.exceptions.SearchException;
+import ca.goudie.advisorinformationscraping.services.scrapers.IScraper;
+import ca.goudie.advisorinformationscraping.services.searchers.ISearcher;
+import ca.goudie.advisorinformationscraping.services.selectors.ScraperSelector;
+import ca.goudie.advisorinformationscraping.services.selectors.SearchServiceSelector;
+import ca.goudie.advisorinformationscraping.services.selectors.WebDriverSelector;
+import ca.goudie.advisorinformationscraping.utils.AisCountryUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.openqa.selenium.WebDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Service
+public class RunService {
+
+	@Autowired
+	private BlacklistService blacklistService;
+
+	@Autowired
+	private ScraperSelector scraperSelector;
+
+	@Autowired
+	private SearchServiceSelector searchServiceSelector;
+
+	@Autowired
+	private WebDriverSelector webDriverSelector;
+
+	public ScrapeResult run() throws SearchException {
+		final WebDriver webDriver = this.webDriverSelector.selectWebDriver();
+		final ISearcher searcher = this.searchServiceSelector.selectSearcher();
+
+		/*
+		final String firm = "Abaco Asset Management LLP";
+		final String city = "London";
+		final String country = "UK";
+		final String firm = "Prosser Knowles Associates LTD";
+		final String city = "Hartlebury";
+		final String country = "UK";
+		*/
+		final String firm = "Wren Sterling Financial Planning LTD";
+		final String city = "";
+		final String country = "";
+
+		String query = firm;
+
+		if (StringUtils.isNotBlank(city)) {
+			if (StringUtils.isNotBlank(country)) {
+				query += " " + city + ", " + country;
+			} else {
+				query += " " + city;
+			}
+		} else if (StringUtils.isNotBlank(country)) {
+			query += " " + country;
+		}
+
+		final String countryCode = AisCountryUtils.findCountryCode(country);
+
+		int resultsLimit = 1;
+
+		final Collection<String> blacklist = this.blacklistService.getBlacklist();
+		final Collection<String> links = searcher.search(webDriver,
+				query,
+				resultsLimit,
+				blacklist);
+		final ScrapeResult out = new ScrapeResult();
+		final Collection<Firm> results = new ArrayList<>();
+
+		for (final String link : links) {
+			final IScraper scraper = this.scraperSelector.selectScraper(link);
+
+			try {
+				results.add(scraper.scrapeWebsite(webDriver, link, countryCode));
+			} catch (ScrapeException e) {
+				e.printStackTrace();
+			}
+		}
+
+		out.getFirms().addAll(results);
+
+		return out;
+	}
+
+}
