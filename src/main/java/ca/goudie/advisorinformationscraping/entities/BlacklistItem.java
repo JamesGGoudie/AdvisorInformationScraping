8bb904719bfc1680fb31a2d7345a191c8ecea932
James Gabriel Goudie <james.g.goudie@gmail.com>
2021-01-19 19:20:45 -0500

    Removed unused imports

--- src/main/java/ca/goudie/advisorinformationscraping/entities/BlacklistItem.java
+++ src/main/java/ca/goudie/advisorinformationscraping/entities/BlacklistItem.java
@@ -1,8 +1,6 @@
 package ca.goudie.advisorinformationscraping.entities;
 
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
