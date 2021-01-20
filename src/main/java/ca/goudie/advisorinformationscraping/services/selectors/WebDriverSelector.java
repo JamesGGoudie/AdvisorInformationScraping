19897e7bee62ad9bbd932c1ddeb4d2909e10c70d
James Gabriel Goudie <james.g.goudie@gmail.com>
2021-01-20 03:47:56 -0500

    Externalized chrome-driver location and modified the WebDriverSelector to create a new one each time

--- src/main/java/ca/goudie/advisorinformationscraping/services/selectors/WebDriverSelector.java
+++ src/main/java/ca/goudie/advisorinformationscraping/services/selectors/WebDriverSelector.java
@@ -3,19 +3,19 @@ package ca.goudie.advisorinformationscraping.services.selectors;
 import ca.goudie.advisorinformationscraping.logging.LoggingWebDriver;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WebDriverSelector {
 
-	private static final String CHROME_KEY = "C";
+	@Value("${selenium.chrome-driver-location}")
+	private String chromeDriverLocation;
 
-	@Autowired
-	@Qualifier("chromeWebDriver")
-	private WebDriver chromeWebDriver;
+	private static final String CHROME_KEY = "C";
 
 	/**
 	 * Selects the default web driver.
@@ -41,7 +41,18 @@ public class WebDriverSelector {
 	}
 
 	private WebDriver getDefault() {
-		return new LoggingWebDriver(this.chromeWebDriver);
+		return new LoggingWebDriver(this.buildChromeWebDriver());
+	}
+
+	private WebDriver buildChromeWebDriver() {
+		// Need to tell Selenium where the chromedriver file is.
+		System.setProperty("webdriver.chrome.driver", this.chromeDriverLocation);
+
+		final ChromeOptions options = new ChromeOptions();
+		// By setting Chrome to headless, we don't need GPU resources.
+		options.setHeadless(true);
+
+		return new ChromeDriver(options);
 	}
 
 }
